65c46f34684bd2271e3b9a283ce40270501f7b84
rzxx <82148134+rzxx@users.noreply.github.com>
2026-02-07 00:48:05 +0300

    cleanup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import wails from "@wailsio/runtime/plugins/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), wails("./bindings")],
+  plugins: [react(), tailwindcss(), wails("./bindings")],
 });
